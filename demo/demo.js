56f51cebbbac4a05b41c95cd26c775b28e8f3595
nange <463499510@qq.com>
2014-04-02 16:30:41 +0800

    update

--- demo/demo.js
+++ demo/demo.js
@@ -1,30 +1,3 @@
 $(function() {
-	$('.carousel-wrapper').carousel({
-		showItemNum		: 4,
-		afterInit: function($children) {
-			var _this = this,
-				$children = _this.$el.children();
-
-			$children.on('click', function(e) {
-				e.stopPropagation();
-				var $this = $(this);
-				var index = $children.index($this);
-
-				_this.to(index);
-				$children.animate({
-					'width': _this.opts.itemWidth
-				}, 150);
-				$this.animate({
-					'width': 2 * _this.opts.itemWidth
-				}, 400);
-			});
-
-			$(document).on('click', function() {
-				$children.animate({
-					'width': _this.opts.itemWidth,
-				});
-			});
-
-		}
-	});
+	$('.carousel-wrapper').carousel();
 });
